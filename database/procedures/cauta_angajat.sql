537785b5c05515eb90bc511c45568665ac2d9b2b
Rares <rcm14052004@gmail.com>
2023-12-10 13:59:41 +0200

    Am adaugat prima procedura din modulul 1

---
+++ database/procedures/cauta_angajat.sql
@@ -0,0 +1,16 @@
+DELIMITER //
+
+CREATE PROCEDURE cauta_angajat(IN param_nume VARCHAR(20), IN param_prenume VARCHAR(20), IN param_denumire_functie VARCHAR(45))
+BEGIN
+
+SELECT *
+FROM (
+	SELECT cnp_angajat FROM angajat
+	INNER JOIN functie ON angajat.id_functie = functie.id_functie AND functie.denumire_functie = param_denumire_functie
+	WHERE angajat.nume = param_nume AND angajat.prenume = param_prenume
+) as angajati_returnati
+INNER JOIN orar ON angajati_returnati.cnp_angajat = orar.cnp_angajat;
+
+END
+//
+DELIMITER ;
