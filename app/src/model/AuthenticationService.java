86ba4ba4014be47cfc50029c28bb430487431679
Bogdan Marcu <bogdanmarcu2003@gmail.com>
2023-12-15 20:06:19 +0200

    Implemented features to support later permission based customization.

--- app/src/model/AuthenticationService.java
+++ app/src/model/AuthenticationService.java
@@ -7,6 +7,12 @@ import exception.IncorrectCredentialsException;
 
 public class AuthenticationService {
 	private boolean isSignedIn = false;
+	private int accType;
+	private String username;
+
+	private static final int ACC_HR = 0;
+	private static final int ACC_FINANCIAL = 1;
+	private static final int ACC_MEDICAL = 2;
 	
 	private DatabaseService databaseService = DatabaseService.getInstance();
 	
@@ -21,15 +27,31 @@ public class AuthenticationService {
 			throw new IncorrectCredentialsException();
 		}
 		
-		System.out.println(dbCredentials.getUsername() + " " + dbCredentials.getPassword());
-		
 		databaseService.connectToDatabaseWithCredentials(dbCredentials);
 		
+		switch(dbCredentials.getUsername())
+		{
+			case "acc_hr":
+				accType = ACC_HR;
+				break;
+			case "acc_financial":
+				accType = ACC_FINANCIAL;
+				break;
+			case "acc_medical":
+				accType = ACC_MEDICAL;
+				break;
+		}
+		username = credentials.getUsername();
 		isSignedIn = true;
 	}
 	
 	public boolean getIsSignedIn()
 	{
 		return isSignedIn; 
+	}
+	
+	public String getUsername()
+	{
+		return username;
 	}
 }
